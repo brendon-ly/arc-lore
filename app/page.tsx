0b358e987d27470bc332e9f8580d1159d8c6122d
brendon-ly <brennyleeying@gmail.com>
2025-11-10 12:41:48 -0400

    Home page styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,28 @@
 export default function Home() {
   return (
     <main>
-    <div className="Categories">
-        <h1>History</h1>
-        <h1>People</h1>
-        <h1>Toledo</h1>
-        <h1>Arc</h1>
-        <h1>Topside</h1>
-    </div>
-    <div className="Welcome">
-        <p>Dive in</p>
+    <div className="relative">
+      <div className="isolate">
+        <div className="flex flex-row justify-evenly pt-6 uppercase text-lg">
+          <h1>History</h1>
+          <h1>People</h1>
+          <h1>Toledo</h1>
+          <h1>Arc</h1>
+          <h1>Topside</h1>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center pt-70 ">
+          <p className="font-bold uppercase text-5xl">Enlist. Resist</p>
+          <div className="text-justify py-2 px-170 text-balance">
+            <p>Generations after a global environmental collapse wiped out most of human society, only a few patches of habitable land remained. Survivors gathered in makeshift communities within the remnants of old cities, as well as in countless small tribes.
+
+              Just as some degree of civilization returned, crude machines started falling from the skies. In the Rust Belt, after a decade of hard fighting, this First Wave of ARC is eventually held back—but not for long.
+
+              Within years, ARC returns with a vengeance. This time, survivors are forced underground, into the makeshift community of Speranza.<br></br>
+              
+              <br></br>But survival is no easy task. Resources are limited, conflicts are endless, and competition for survival is fierce. True survival means risking the surface to recover supplies.</p>
+          </div>
+      </div>
     </div>
     </main>
   );
