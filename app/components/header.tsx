35f8a7019b333ebffd81df1c16e80e5e8d2c10cb
brendon-ly <brennyleeying@gmail.com>
2025-11-10 15:03:52 -0400

    Makes header sticky

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="isolate border-t-2">
+        <header className="isolate border-t-2 sticky top-0">
             <div className="mx-auto w-fit">
                 <div className="flex flex-row gap-x-20 justify-evenly pt-6 uppercase text-lg static">
                     <div className="inline-block h-[30px] min-h-[1em] w-0.5 self-stretch bg-neutral-100 dark:bg-white/10"></div>
