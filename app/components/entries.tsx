6f68f307921c3508dda606abc79de13b0eda2730
brendon-ly <brennyleeying@gmail.com>
2025-11-27 14:39:35 -0400

    Better responsiveness for small screens

--- app/components/entries.tsx
+++ app/components/entries.tsx
@@ -10,12 +10,12 @@ export default function Entries({name}: EntriesProps) {
     return (
   <>
     <div className="flex flex-col lg:flex-row gap-6
-                    ml-4 md:ml-10 lg:ml-102
-                    mr-110 md:mr-100
+                    ml-4 sm:ml-4 md:ml-10 lg:ml-102
+                    mr-4 sm:mr-4 md:mr-90
                     mt-6">
-      <div className="w-full lg:w-58 shrink-0
+      <div className="w-full sm:w-40 lg:w-58
                       border-r
-                      h-[52vh] md:h-[52vh] 
+                      h-[20vh] sm:h-[30vh] md:h-[52vh]
                       overflow-auto no-scrollbar">
         <EntriesList name = {name} />
       </div>
